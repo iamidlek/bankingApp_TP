2cd93e64c2af7a85d886ce6f085f7ffe38876169
iamidlek <iamidlek@naver.com>
2021-09-13 23:17:34 +0900

    babel config

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,11 @@ module.exports = {
           'sass-loader',
         ]
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
     ]
   },
   plugins: [
