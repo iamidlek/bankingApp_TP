064c0d26360d51ba98cf4beec22e24b60ab52109
iamidlek <iamidlek@naver.com>
2021-09-13 22:49:15 +0900

    정적파일도 가져오기위한 copy 플러그인

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: './src/main.js',
@@ -11,6 +12,11 @@ module.exports = {
   plugins: [
     new HtmlPlugin({
       template: './index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'static' }
+      ]
     }),
   ],
   devServer: {
